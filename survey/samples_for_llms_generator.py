d0734152b6a0130079f790663513f269c1eed9a7
Zuzanna Pietrzak <zpietrza@akamai.com>
2025-06-12 21:21:35 +0200

    survey with llms evaluation

--- survey/samples_for_llms_generator.py
+++ survey/samples_for_llms_generator.py
@@ -7,17 +7,17 @@ with open('samples_for_survey.jsonl', 'r') as file:
     lines = file.readlines()
     for line in lines:
         sample = json.loads(line)
-        label = 'Generated' if sample['label'] == 1 else 'Real'
+        label = 'GENERATED' if sample['label'] == 1 else 'REAL'
         samples.append((sample['text'], label))
 
 random.shuffle(samples)
 
-with open('samples_for_chatgpt.txt', 'w') as file:
+with open('samples_for_llms.txt', 'w') as file:
     for i, sample in enumerate(samples, start=1):
         text, label = sample
         labels.append(label)
         file.write(f"{i}. {text}\n")
 
-with open('labels_for_chatgpt.txt', 'w') as file:
+with open('samples_for_llms_labels.txt', 'w') as file:
     for i, label in enumerate(labels, start=1):
         file.write(f"{i}. {label}\n")
